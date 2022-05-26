add9909ace57bdbf14291bf6585300fb5a9177a9
Eburu Dearie <eburujacintha@gmail.com>
2022-05-26 11:24:36 +0200

    commit on 'master'

---
+++ form.js
@@ -0,0 +1,19 @@
+//Email Validation
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.email_address.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.email_address.focus();
+return false;
+}
+}
+
+
